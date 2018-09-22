6e075b37e2ab9ba8eacfa3d18b983dd7062ebbfa
Vince Shao <vinceshao1992@gmail.com>
2018-09-22 10:03:42 -0400

    [server] add history api fallback

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
 const app = express()
+const port = process.env.PORT || 5000
 
 app.use(serveStatic(path.join(__dirname, 'dist')))
-const port = process.env.PORT || 5000
+app.use(history())
 app.listen(port)
