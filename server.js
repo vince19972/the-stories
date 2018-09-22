b7565d4a65ab2a34a401ee727e060f2b1d444954
Vince Shao <vinceshao1992@gmail.com>
2018-09-22 13:48:51 -0400

    [server] update server config for history redirect

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express')
-const serveStatic = require('serve-static')
 const path = require('path')
-const app = express()
 const history = require('connect-history-api-fallback')
-const port = process.env.PORT || 5000
 
-app.use(serveStatic(path.join(__dirname, 'dist')))
-app.use(history())
-app.listen(port)
+const app = express()
+
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'))
+
+app.use(staticFileMiddleware)
+app.use(history({
+  disableDotRule: true,
+  verbose: true
+}))
+app.use(staticFileMiddleware)
+
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/dist/index.html'))
+})
+
+var server = app.listen(process.env.PORT || 5000, function () {
+  var port = server.address().port
+  console.log("App now running on port", port)
+})
