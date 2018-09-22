938df409edfa23c67ded54d1223123bb17b7465d
Vince Shao <vinceshao1992@gmail.com>
2018-09-22 14:45:51 -0400

    [angers] category page style tuning

--- src/views/Angers.vue
+++ src/views/Angers.vue
@@ -5,7 +5,7 @@
       <div class="phrases">
         <router-link
           class="phrase"
-          v-for="phrase in phrases" :key="phrase.id"
+          v-for="phrase in categories" :key="phrase.id"
           :to="'/angers/' + phraseLink(phrase)"
         >
         {{ phrase }}
@@ -16,16 +16,16 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 
 export default {
   name: 'angers',
-  data () {
-    return {
-      phrases: [
-        'gun control', 'corruption', 'refugee', 'trump', 'fake news', 'captalism', 'global warming', 'poverty', 'media',
-        'facebook', 'poverty', 'beauracracy', 'drugs', 'racist', 'air pollution'
-      ],
-      phrasesLinks: []
+  computed: {
+    ...mapState({
+      storiesStore: state => state.anger.stories
+    }),
+    categories () {
+      return this.storiesStore.map(story => story.category.replace('-', ' '))
     }
   },
   methods: {
@@ -41,6 +41,8 @@ export default {
 
   .cover {
     position: fixed;
+    top: 0;
+    left: 0;
     height: 100vh;
     width: 100vw;
     background: linear-gradient(to bottom, transparent 0%, rgba(189,189,189,0) 26%, var(--c-default) 100%);
@@ -51,11 +53,11 @@ export default {
     flex-wrap: wrap;
   }
   .phrase {
-    font-size: 6vw;
+    font-size: 5.5vw;
     font-weight: var(--f-bold);
     color: var(--c-f-default);
     margin-bottom: 1vw;
-    margin-right: 3vw;
+    margin-right: 3.5vw;
     transition: .5s;
     &:last-child {
       margin-right: 0;
@@ -68,11 +70,11 @@ export default {
     @media(--below-medium) { margin-bottom: 1.5vw; }
     @media(--below-desktop) {
       margin-bottom: 2vw;
-      margin-right: 4vw;
+      margin-right: 4.5vw;
     }
     @media(--below-tablet) { font-size: 8vw; }
     @media(--below-mobile) {
-      font-size: 6vw;
+      font-size: 7vw;
       margin-bottom: 2.5vw;
     }
   }
