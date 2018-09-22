a2004ba3423bce76086d06ffdec82b1ce8cda7f4
Vince Shao <vinceshao1992@gmail.com>
2018-09-22 09:38:38 -0400

    [server] package script update

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
-// server.js
-var express = require('express')
-var app = express()
-var path = require('path')
-var serveStatic = require('serve-static')
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
 
 app = express()
-app.use(serveStatic(`${__dirname}/dist`))
-var port = process.env.PORT || 5000
+app.use(serveStatic(path.join(__dirname, 'dist')))
+const port = process.env.PORT || 80
 app.listen(port)
-
-console.log('server started ' + port)
