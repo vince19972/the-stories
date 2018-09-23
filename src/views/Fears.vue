741e19a4657890c250ad5be5afc9f1ddb32a5dae
Vince Shao <vinceshao1992@gmail.com>
2018-09-22 23:40:25 -0400

    [fears] add touch event

--- src/views/Fears.vue
+++ src/views/Fears.vue
@@ -1,16 +1,21 @@
 <template>
-  <div class="container">
-    <div class="content-margin -s">
-      <div class="wrapper">
-        <div :class="['phrases', {'-is-white': isWhite}]">
-          <p v-for="phrase in phrasesStore" :key="phrase.id" class="phrase" style="opacity: 0.1;">{{ phrase }}</p>
-        </div>
-        <div :class="['unity-wrapper', isWhite ? '-is-show' : '-is-hidden']">
-          <h1 class="unity">UNITY</h1>
+  <v-touch
+    @pan="scrollEvent()"
+    @swipe="scrollEvent()"
+  >
+    <div class="container">
+      <div class="content-margin -s">
+        <div class="wrapper">
+          <div :class="['phrases', {'-is-white': isWhite}]">
+            <p v-for="phrase in phrasesStore" :key="phrase.id" class="phrase" style="opacity: 0.1;">{{ phrase }}</p>
+          </div>
+          <div :class="['unity-wrapper', isWhite ? '-is-show' : '-is-hidden']">
+            <h1 class="unity">UNITY</h1>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </v-touch>
 </template>
 
 <script>
@@ -29,8 +34,56 @@ export default {
     GradientCover
   },
   mounted () {
-    document.getElementsByTagName('body')[0].addEventListener('mousewheel', event => {
+    document.getElementsByTagName('body')[0].addEventListener('mousewheel', event => this.scrollEvent(event))
+  },
+  beforeDestroy () {
+    const body = document.getElementsByTagName('body')[0]
+    body.removeEventListener('mousewheel', this.scrollEvent)
+    body.style.backgroundColor = 'black'
+    this.$store.state.isUnity = false
+  },
+  computed: {
+    ...mapState({
+      phrasesStore: state => state.fear.phrases,
+      isUnity: state => state.isUnity
+    })
+  },
+  methods: {
+    test (text) {
+      alert(text)
+    },
+    getRandomIntInclusive (min, max) {
+      min = Math.ceil(min)
+      max = Math.floor(max)
+      const genNum = () => Math.floor(Math.random() * (max - min + 1)) + min
+      const isDuplicated = num => this.pickedPhraseId.filter(e => e === num).length > 0
+      let ranNum = genNum()
+
+      while (isDuplicated(ranNum)) {
+        ranNum = genNum()
+      }
+
+      return ranNum
+    },
+    updateOpacity (phrases) {
       // const isDown = event.deltaY >= 0
+      let randomNum = this.getRandomIntInclusive(0, this.phrasesStore.length - 1)
+      this.pickedPhraseId.push(randomNum)
+
+      const pickedPhrase = phrases[randomNum]
+      pickedPhrase.style.opacity = 1
+    },
+    updateBackground (percent, max = false) {
+      const body = document.getElementsByTagName('body')[0]
+      const rgbaVal = 2.55 * percent
+      if (!max) {
+        body.style.backgroundColor = `rgb(${rgbaVal}, ${rgbaVal}, ${rgbaVal})`
+      } else {
+        body.style.backgroundColor = 'white'
+      }
+    },
+    scrollEvent (event) {
+      console.log(event)
       const isFull = () => this.phrasesStore.length - this.pickedPhraseId.length <= 1
       const lightenedPercent = () => Math.floor((this.pickedPhraseId.length / this.phrasesStore.length) * 100)
       const phrases = document.getElementsByClassName('phrase')
@@ -47,48 +100,6 @@ export default {
         } else {
           this.updateBackground(lightenedPercent())
         }
-      }
-    })
-  },
-  beforeDestroy () {
-    document.getElementsByTagName('body')[0].removeEventListener('mousewheel')
-    this.$store.state.isUnity = false
-    document.getElementsByTagName('body')[0].style.backgroundColor = 'black'
-  },
-  computed: {
-    ...mapState({
-      phrasesStore: state => state.fear.phrases,
-      isUnity: state => state.isUnity
-    })
-  },
-  methods: {
-    getRandomIntInclusive (min, max) {
-      min = Math.ceil(min)
-      max = Math.floor(max)
-      const genNum = () => Math.floor(Math.random() * (max - min + 1)) + min
-      const isDuplicated = num => this.pickedPhraseId.filter(e => e === num).length > 0
-      let ranNum = genNum()
-
-      while (isDuplicated(ranNum)) {
-        ranNum = genNum()
-      }
-
-      return ranNum
-    },
-    updateOpacity (phrases) {
-      let randomNum = this.getRandomIntInclusive(0, this.phrasesStore.length - 1)
-      this.pickedPhraseId.push(randomNum)
-
-      const pickedPhrase = phrases[randomNum]
-      pickedPhrase.style.opacity = 1
-    },
-    updateBackground (percent, max = false) {
-      const body = document.getElementsByTagName('body')[0]
-      const rgbaVal = 2.55 * percent
-      if (!max) {
-        body.style.backgroundColor = `rgb(${rgbaVal}, ${rgbaVal}, ${rgbaVal})`
-      } else {
-        body.style.backgroundColor = 'white'
       }
     }
   }
@@ -116,10 +127,21 @@ export default {
   }
   .phrase {
     display: inline-block;
-    font-size: 2vw;
-    line-height: 2.5vw;
+    font-size: 2.5vw;
+    line-height: 3vw;
     margin-right: 1.5vw;
     transition: 1.5s;
+
+    @media(--below-desktop) {
+      font-size: 3vw;
+      line-height: 4vw;
+      margin-right: 2vw;
+    }
+    @media(--below-tablet) {
+      font-size: 3.5vw;
+      line-height: 5vw;
+      margin-right: 2.5vw;
+    }
   }
 
   .unity-wrapper {
